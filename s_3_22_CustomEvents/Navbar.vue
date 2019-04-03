705b899e43bd9cdef9a39095b8fe034b3fd53b84
MotoHusarZ <emilsamson@interia.pl>
2019-04-03 20:30:34 +0200

    s_3_22_CustomEvents

---
+++ s_3_22_CustomEvents/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+<nav>
+  <h3>MENU:</h3>
+ <ul>
+   <li><a href="#">Home </a></li>
+   <li><a href="#">About</a></li>
+   <li><a href="#">Contact</a></li>
+ </ul>
+</nav>
+
+</template>
+
+
+<script>
+  export default {
+    name: 'Navbar',
+    data(){
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!--we could write
+<style scoped> if we wouldn't like this style to be exported to the App.vue
+  -->
+<style>
+
+nav {
+  text-align: center;
+}
+
+nav ul {
+  padding: 0;
+}
+nav li {
+  display: inline-block;
+  list-style-type: none;
+  margin: 0;
+}
+
+a {
+  color: white;
+}
+</style>
---
+++ s_3_22_CustomEvents/OnlineFriends.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="online-friends">
+    <h2>Online Friends </h2>
+
+    <div v-for="(friend, index) in friends" :key:="index">
+        <span v-if="friend.online">{{ friend.name }}</span>
+
+      </div>
+      <br>
+      Fooling around content: <br>
+       {{ gorky }} <br> {{ pureText }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'OnlineFriends',
+  props:  ['friends' , 'gorky', 'pureText'],
+  data() {
+    return {
+
+
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
