c8f5c6c155ab7141abfec1aca6a0e6ae443b429e
MotoHusarZ <emilsamson@interia.pl>
2019-04-07 21:59:37 +0200

    s_4_29_routerLinks

---
+++ s_4_29_routerLinks/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #c8ccd0;
+  margin-top: 60px;
+}
+
+body {
+  padding:5px 15px 15px 15px;
+  font-size: 87.5%;
+  font-weight: normal;
+  /*color:#93A1AD;*/
+  /*background: linear-gradient(#C0C0C0, #C0C0C0, #C0C0C0, #ADD8E6, #66CDAA);*/
+  /*background: url("bkg-blu.jpg"); */
+   background: #002B36;
+   /*background: red;*/
+
+}
+
+
+</style>
